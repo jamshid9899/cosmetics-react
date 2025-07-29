275c637591f4e1737fd1cce056a52b4ccab8cf68
jakhongiromonboev <jakhongiromonboev@gmail.com>
2025-07-29 20:25:22 +0900

    feat: develop active users sectional component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,10 +1,65 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import Card from "@mui/joy/Card";
+import Typography from "@mui/joy/Typography";
+import { CssVarsProvider } from "@mui/joy/styles";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+
+const activeUsers = [
+  { memberNick: "Martin", memberImage: "/img/martin.webp" },
+  { memberNick: "Justin", memberImage: "/img/justin.webp" },
+  { memberNick: "Rose", memberImage: "/img/rose.webp" },
+  { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
+];
 
 export default function ActiveUsers() {
   return (
-    <div>
-      <Container>ActiveUsers</Container>
+    <div className="active-users-frame">
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Active Users</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((ele, index) => {
+                  return (
+                    <Card key={index} variant="outlined" className={"card"}>
+                      <CardOverflow variant="soft">
+                        <AspectRatio ratio="1">
+                          <img src={ele.memberImage} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
+
+                      <CardOverflow
+                        variant="soft"
+                        sx={{
+                          justifyContent: "center",
+                          display: "flex",
+                          py: 1,
+                          backgroundColor: "#FFF",
+                        }}
+                      >
+                        <Typography
+                          level="body-sm"
+                          textAlign="center"
+                          fontWeight="600"
+                          fontSize="12px"
+                          textColor={"#000"}
+                        >
+                          {ele.memberNick}
+                        </Typography>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className={"no-data"}>Active users are not available</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
