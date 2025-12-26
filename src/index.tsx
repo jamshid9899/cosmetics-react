9903d0e130da85cf1e924122306811a00f6e8c3a
jamshid98 <jamshid98991213@gmail.com>
2025-12-27 00:56:59 +0900

    feat: develop Socket.io Client

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import theme from "./app/MaterialTheme";
 import "./css/index.css";
 import ContextProvider from "./app/context/ContextProvider";
 import App from "./app/App";
+import { SocketProvider } from "./app/context/SocketContext";
 
 const container = document.getElementById("root")!; //RealDom
 const root = createRoot(container);
@@ -18,12 +19,14 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ContextProvider>
+        <SocketProvider>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Router>
             <App />
           </Router>
         </ThemeProvider>
+        </SocketProvider>
       </ContextProvider>
     </Provider>
   </React.StrictMode>
